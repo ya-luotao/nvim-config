200c6319f851d314a022ec3ab8706da0889d7c83
ya-luotao <luotao@hey.com>
2022-05-20 22:44:01 +0800

    Modify keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -14,16 +14,16 @@ keymap('n', '<C-t>', ':NERDTreeToggle<CR>', opts)
 keymap('n', '<C-f>', ':NERDTreeFind<CR>', opts)
 
 -- Buffers
-keymap('n', '<leader>p', ':BufferPrevious<CR>', opts)
-keymap('n', '<leader>n', ':BufferNext<CR>', opts)
+keymap('n', '<C-p>', ':BufferPrevious<CR>', opts)
+keymap('n', '<C-n>', ':BufferNext<CR>', opts)
 keymap('n', '<leader>P', ':BufferMovePrevious<CR>', opts)
 keymap('n', '<leader>N', ':BufferMoveNext<CR>', opts)
 keymap('n', '<leader>x', ':BufferClose<CR>', opts)
 
 -- Telescope
-keymap('n', '<leader>F', ':Telescope find_files<CR>', opts)
-keymap('n', '<leader>G', ':Telescope live_grep<CR>', opts)
-keymap('n', '<leader>B', ':Telescope buffers<CR>', opts)
+keymap('n', '<leader>f', ':Telescope find_files<CR>', opts)
+keymap('n', '<leader>g', ':Telescope live_grep<CR>', opts)
+keymap('n', '<leader>b', ':Telescope buffers<CR>', opts)
 
 -- Resize
 keymap("n", "<leader>_", ":resize -2<CR>", opts)
